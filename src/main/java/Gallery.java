0f6f9f2400d9aba6b4d54460bff9a6b3c9764b84
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-04-29 11:26:21 +0100

    add constructors to gallery

--- src/main/java/Gallery.java
+++ src/main/java/Gallery.java
@@ -4,6 +4,11 @@ public class Gallery {
 
     private String name;
     private double till;
-    private ArrayList<Artwork>();
+    private ArrayList<Artwork> artworks;
 
+    public Gallery(String name, double till) {
+        this.name = name;
+        this.artworks = new ArrayList<>();
+        this.till = 0;
+    }
 }
