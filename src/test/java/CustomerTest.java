75e53b2771098f8dced055b6264fad3f6055d7f1
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-04-29 12:08:21 +0100

    create a BeforeEach test

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,7 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+
 public class CustomerTest {
 
     Customer customer;
     Artist vanGogh;
     Artwork sunflowers;
+
+    @BeforeEach
+    public void setUp(){
+        customer = new Customer("John Doe", 800);
+
+        vanGogh = new Artist("Van Gogh");
+        sunflowers = new Artwork("Sunflowers", vanGogh, 1000, 766);
+    }
+
 
 }
