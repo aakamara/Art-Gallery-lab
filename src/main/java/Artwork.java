829058f5865cc1f4a5475e4ca5df05f4273a86d1
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-04-29 11:50:17 +0100

    add getters & setters to artwork

--- src/main/java/Artwork.java
+++ src/main/java/Artwork.java
@@ -6,11 +6,45 @@ public class Artwork {
     private int nft;
 
     //constructor
-
     public Artwork(String title, Artist artist, double price, int nft) {
         this.title = title;
         this.artist = artist;
         this.price = price;
+        this.nft = nft;
+    }
+
+
+    //getters & setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getNft() {
+        return nft;
+    }
+
+    public void setNft(int nft) {
         this.nft = nft;
     }
 }
