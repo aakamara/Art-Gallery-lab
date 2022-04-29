d36ef86c571ec1d2c9246ebd1dbc55312d6c2081
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-04-29 11:11:22 +0100

    implemented constructor, getters & setters

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class Customer {
+
+    private String name;
+    private double wallet;
+    private ArrayList<Artwork> customerArtwork;
+
+    // Constructor -- command + N --
+    public Customer(String name, double wallet){
+        this.name = name;
+        this.wallet = wallet;
+        this.customerArtwork = new ArrayList<>();
+    }
+
+    // Getters & Setters -- command + N --
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(double wallet) {
+        this.wallet = wallet;
+    }
+
+    public ArrayList<Artwork> getCustomerArtwork() {
+        return customerArtwork;
+    }
+
+    public void setCustomerArtwork(ArrayList<Artwork> customerArtwork) {
+        this.customerArtwork = customerArtwork;
+    }
 }
